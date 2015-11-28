2202a628b8d31ececa84fe1c8d14eadc44d39ee9
l0sg <tkdrlf9202@gmail.com>
2015-11-27 18:00:20 -0800

    151027 cifar-10 data extract & unpickle

--- data_module.py
+++ data_module.py
@@ -1,7 +1,19 @@
-def unpickle(file):
+def extract_file():
+    import os
+    import tarfile
+    filepath = os.path.join(os.path.realpath('..'), "data", 'cifar-10-python.tar.gz')
+    file = tarfile.open(filepath)
+    file.extractall()
+
+
+def unpickle(filename):
+    import os
     import cPickle
-    fo = open(file, 'rb')
-    dict = cPickle.load(fo)
-    fo.close()
+    dict=[]
+    for name in filename:
+        filepath=os.path.join(os.path.realpath(''), "cifar-10-batches-py", name)
+        fo=open(filepath, 'rb')
+        dict.append(cPickle.load(fo))
+        fo.close()
     return dict
 
--- run_classifier.py
+++ run_classifier.py
@@ -1,5 +1,18 @@
 import data_module as dm
+import os
 import tensorflow as tf
 import numpy as np
 
-cifar=dm.unpickle()
+# Check if there already exists the extracted files
+if not os.path.isdir(os.path.join(os.path.realpath(''), "cifar-10-batches-py")):
+    dm.extract_file()
+
+# Unpickle batches.meta file and save variables
+meta = dm.unpickle(['batches.meta'])
+label_names = meta[0]['label_names']
+num_cases_per_batch = meta[0]['num_cases_per_batch']
+
+# Unpickle data_match files
+data_batches = dm.unpickle(['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5'])
+
+print("a")
