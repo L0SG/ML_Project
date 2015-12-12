a412aa8ee4fff67ecaddba161b167c3dfee362e5
l0sg <tkdrlf9202@gmail.com>
2015-12-12 20:12:56 +0900

    extract all patches fnc added

--- data_module.py
+++ data_module.py
@@ -17,3 +17,30 @@ def unpickle(filename):
         fo.close()
     return dict
 
+
+def load_train_data_all(data_batches):
+    import numpy as np
+    trainX=[]
+    trainY=[]
+    for i in range(0, 5):
+        trainX.append(data_batches[i]['data'])
+        trainY.append(data_batches[i]['labels'])
+    trainX=np.concatenate(trainX)
+    trainY=np.concatenate(trainY)
+    return trainX, trainY
+
+
+def extract_all_patches(trainX, rf_size, step_size):
+    import numpy as np
+    print("extracting all possible patches from data...")
+    patches = []
+    iter = 0
+    for image in trainX:
+        image_reshaped = np.reshape(image, (3, 32, 32))
+        for i in range(0, 32-rf_size, step_size):
+            for j in range(0, 32-rf_size, step_size):
+                patches.append(image_reshaped[:][i:i+rf_size][j:j+rf_size])
+        iter+=1
+        if divmod(iter, 10000)[1] == 0:
+            print(str(iter)+" of "+str(len(trainX))+" data extracted...")
+    return patches
--- run_classifier.py
+++ run_classifier.py
@@ -1,7 +1,15 @@
 import data_module as dm
 import os
-import tensorflow as tf
 import numpy as np
+import theano as th
+
+# Variables (adapted from demo code of the paper)
+rf_size = 6
+step_size = 1
+num_centroids=1600
+whitening='true'
+num_patches = 400000
+CIFAR_DIM = 32 * 32 * 3
 
 # Check if there already exists the extracted files
 if not os.path.isdir(os.path.join(os.path.realpath(''), "cifar-10-batches-py")):
@@ -12,7 +20,16 @@ meta = dm.unpickle(['batches.meta'])
 label_names = meta[0]['label_names']
 num_cases_per_batch = meta[0]['num_cases_per_batch']
 
-# Unpickle data_match files
+# Unpickle data_batch files
 data_batches = dm.unpickle(['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5'])
 
-print("a")
+# load all train data
+trainX, trainY = dm.load_train_data_all(data_batches)
+
+# extract all patches
+patches = dm.extract_all_patches(trainX, rf_size, step_size)
+
+print("break")
+
+
+
